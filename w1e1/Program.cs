d9a0278f3ae00c8c3aaf17f9ee6dbf6685452808
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-03-24 20:53:04 -0300

    add: week 1 execice 1

---
+++ w1e1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/* CONSIGNA: El precio de los productos no tiene incluido el incremento del 15% que representa la ganancia del negocio.
+ * El algoritmo debe calcular el precio final a abonar por un cliente.
+ * Se ingresa el precio del producto y como resultado muestra el precio de venta. */
+
+namespace w1e1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double UTILITY = 0.15;
+            double basicPrice = 0;
+            double priceWUtilities = 0;
+            Console.Write("Ingresar el precio del producto: $");
+            basicPrice = float.Parse( Console.ReadLine() );
+            priceWUtilities = basicPrice * (1 + UTILITY);
+            Console.WriteLine("El precio final es: $" + priceWUtilities);
+        }
+    }
+}
