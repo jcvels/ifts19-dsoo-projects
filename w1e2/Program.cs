b4c9d57615b2c7d761452189dd76f3d387a46819
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-03-24 22:38:36 -0300

    add: week 1 execise 2

---
+++ w1e2/Program.cs
@@ -0,0 +1,33 @@
+﻿/* En el campeonato estudiantil llegan a la última etapa los participantes que superen los 80 puntos.
+ * El algoritmo pide el nombre de cada participante y la puntuación de los 3 jurados.
+ * Debe mostrar el nombre y el puntaje total de cada participante.
+ * Nota: cuando se ingrese "FIN" en el nombre del participante finaliza nuestro programa. */
+
+using System;
+
+namespace w1e2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int JUDGE_QTTY = 3;
+            string EXIT_STATMENT = "FIN";
+            string name = "";
+            int pointsAcumulate = 0;
+            while(name != EXIT_STATMENT)
+            {
+                Console.Write("Ingrese el nombre del participante: ");
+                name = Console.ReadLine().ToUpper();
+                if(name != EXIT_STATMENT)
+                {
+                    for (int i = 0; i < JUDGE_QTTY; i++) {
+                        Console.Write("Ingrese el puntaje del juez #" + i + ": ");
+                        pointsAcumulate += int.Parse( Console.ReadLine() );
+                    }
+                    Console.WriteLine("Participante: " + name + " Puntaje: " + pointsAcumulate + "\n\n");
+                }
+            }
+        }
+    }
+}
