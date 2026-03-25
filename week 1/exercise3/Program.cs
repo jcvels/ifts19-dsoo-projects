a50503964459585dfe32eab9d14d04e4cfd4a1f9
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-03-24 23:13:44 -0300

    add: week 1 exercise 3

--- week 1/exercise3/Program.cs
+++ week 1/exercise3/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Modifica la salida de la resolución del punto 2.
+ * Se pide mostrar un mensaje que indique si cada participante llegó a la última etapa o no. */
+
+using System;
 
 namespace exercise3
 {
@@ -10,6 +9,33 @@ namespace exercise3
     {
         static void Main(string[] args)
         {
+            int JUDGE_QTTY = 3;
+            string EXIT_STATMENT = "FIN";
+            int REQUIRED_POINTS = 80;
+            string name = "";
+            string message = "";
+            int pointsAcumulate = 0;
+            while (name != EXIT_STATMENT)
+            {
+                Console.Write("Ingrese el nombre del participante: ");
+                name = Console.ReadLine().ToUpper();
+                if (name != EXIT_STATMENT)
+                {
+                    for (int i = 0; i < JUDGE_QTTY; i++)
+                    {
+                        Console.Write("Ingrese el puntaje del juez #" + i + ": ");
+                        pointsAcumulate += int.Parse(Console.ReadLine());
+                    }
+                    message = "Participante: " + name + " Puntaje: " + pointsAcumulate + " Ultima etapa: ";
+                    if(pointsAcumulate >= REQUIRED_POINTS) {
+                        message += "SI";
+                    }
+                    else {
+                        message += "NO";
+                    }
+                    Console.WriteLine(message + "\n\n");
+                }
+            }
         }
     }
 }
