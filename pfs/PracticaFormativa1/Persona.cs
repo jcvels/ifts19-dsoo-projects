b1c3f000d9621855c545504511b792f579431259
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-03-29 14:37:31 -0300

    add: Persona class

---
+++ pfs/PracticaFormativa1/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaFormativa1
+{
+    internal class Persona
+    {
+        private string nombre;
+        private string domicilio;
+
+        public Persona(string nombre, string domicilio)
+        {
+            this.nombre = nombre;
+            this.domicilio = domicilio;
+        }
+        public string Nombre
+        {
+            set { this.nombre = value; }
+            get { return this.nombre; }
+        }
+        public string Domicilio
+        {
+            set { this.domicilio = value; }
+            get { return this.nombre; }
+        }
+    }
+}
