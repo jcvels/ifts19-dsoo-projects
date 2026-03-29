06bd493ffc41bb6473ffd9d6b6cdce8468f4ab7a
Jorge Pauvels <jcvels@uvcoding.com.ar>
2026-03-29 14:38:04 -0300

    add: Domicilio class

---
+++ pfs/PracticaFormativa1/Domicilio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaFormativa1
+{
+    internal class Domicilio
+    {
+        private string calle;
+        private int numero;
+        private string barrio;
+        public Domicilio(string calle, int numero, string barrio)
+        {
+            this.calle = calle;
+            this.numero = numero;
+            this.barrio = barrio;
+        }
+        public string Calle
+        {
+            get { return this.calle; }
+        }
+        public int Numero
+        {
+            get { return this.numero; }
+        }
+        public string Barrio
+        {
+            get { return this.barrio; }
+        }
+    }
+}
